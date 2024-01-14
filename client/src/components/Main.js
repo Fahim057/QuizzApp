5e74c2037c704cac4eab11802eb7d578817f9381
Fahim Muntasir <fahimmuntasir115@gmail.com>
2024-01-14 13:36:37 +0600

    dd

---
+++ client/src/components/Main.js
@@ -0,0 +1,48 @@
+import React, { useRef } from 'react'
+import { useDispatch } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { setUserId } from '../redux/result_reducer'
+import '../styles/Main.css'
+
+
+export default function Main() {
+
+    const inputRef = useRef(null)
+    const dispatch = useDispatch()
+
+    function startQuiz(){
+        if(inputRef.current?.value){
+            dispatch(setUserId(inputRef.current?.value))
+        }
+    }
+
+  return (
+    <div className='container'>
+        <h1 className='title text-light'>Quiz Application</h1>
+        
+        <ol>
+            <li>You will be asked 10 questions one after another.</li>
+            <li>10 points is awarded for the correct answer.</li>
+            <li>Each question has 4 options. You can choose only one options.</li>
+            <li>You can review and change answers before the quiz finish.</li>
+            <li>The result will be declared at the end of the quiz.</li>
+        </ol>
+
+        <form id="form">
+            <input ref={inputRef} type="text" className='userid' placeholder='Username*' />
+        </form>
+
+        <div className='start'>
+             <Link className='btn' to={'quiz'} onClick={startQuiz}>Start Quiz</Link>
+        </div>
+        
+        <div class="footer">
+        <div class="footer-text">
+            <p>Copyright &copy; 2024 by Md. Ariful Islam | All Rights Received</p>
+        </div>    
+    </div>
+ </div>
+    
+    
+  )
+}
---
+++ client/src/database/data.js
@@ -0,0 +1,54 @@
+export default [
+    {
+        id: 1,
+        question : "Who invented C++?",
+        options : [
+            'Dennis Ritchie',
+            'Ken Thompson',
+            'Brian Kernighan',
+            'Bjarne Stroustrup',
+        ]
+    },
+    {
+        id: 2,
+        question : "Which of the following is correct about this pointer in C++?",
+        options : [
+            'this pointer is passed as a hidden argument in all static variables of a class()',
+            'this pointer is passed as a hidden argument in all the functions of a class',
+            'this pointer is passed as a hidden argument in all non-static functions of a class',
+            'this pointer is passed as a hidden argument in all static functions of a class',
+        ]
+    },
+    {
+        id: 3,
+        question : "Which of the following type is provided by C++ but not C?",
+        options : [
+            'double',
+            'float',
+            'int',
+            'bool',
+        ]
+    },
+    {
+        id: 4,
+        question : "Which concept allows you to reuse the written code in C++?",
+        options : [
+            'Inheritance',
+            'Polymorphism',
+            'Abstraction',
+            'Encapsulation',
+        ]
+    },
+    {
+        id: 5,
+        question : "Which of the following is a correct identifier in C++?",
+        options : [
+            'VAR_1234',
+            '$var_name',
+            '7VARNAME',
+            '7var_name',
+        ]
+    }
+]
+
+export const answers = [3, 2, 3, 0, 0];
